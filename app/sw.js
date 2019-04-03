9d1beab93c87f74d10eb2cc9ffec27e54a383d61
Ethan <15554@twynhamschool.com>
2019-04-03 18:06:27 +0100

    webapp test 1

--- app/sw.js
+++ app/sw.js
@@ -1,20 +1,22 @@
 importScripts('/cache-polyfill.js');
 
 
-self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open('Chaboi').then(function (cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/index.html?homescreen=1',
-        '/?homescreen=1',
-        '/css/style.css',
-        'js/main.js',
-      ]);
+self.addEventListener('fetch', function (event) {
+
+  console.log(event.request.url);
+
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+
+      return response || fetch(event.request);
+
     })
+
   );
+
 });
+
+
 self.addEventListener('fetch', function (event) {
 
   console.log(event.request.url);
